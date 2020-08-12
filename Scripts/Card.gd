9ccbb8dec43e3ab0669ce3a01631b947f7f73d4f
Paul <paul.brancieq@epitech.eu>
2020-08-12 19:09:33 +0400

    Add texture field in card json and script to handle it.

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -6,6 +6,7 @@ func _ready():
 	var card_json = load_card(card)
 	if card_json != null:
 		get_node("Title").set_text(card_json["name"])
+		load_texture(card_json)
 	
 func load_card(name):
 	var file = File.new()
@@ -20,3 +21,13 @@ func load_card(name):
 			get_tree().quit()
 		else:
 			return json.result
+
+func load_texture(card_json):
+	var texture = ImageTexture.new()
+	if ("texture" in card_json):
+		if texture.load("res://Assets/" + card_json["texture"] + ".png") != OK:
+			push_error("[Error] Opening File failed (" + "res://Assets/" + card_json["texture"] + ".png" + ")")
+			get_tree().quit()
+		else:
+			return(get_node("Sprite").set_texture(texture))
+		return (false)
