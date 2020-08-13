aa2a7c273668fefc00095ebeac52ea5458476e09
Paul <paul.brancieq@epitech.eu>
2020-08-13 16:27:56 +0400

    Modify script for card.
    Optimisation + the name of the card appear only if texture is not found + add Attack and defense in the cards json files.

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
 export var card = "sylkabe"
+var card_json = null
 
 func _ready():
-	var card_json = load_card(card)
+	card_json = load_card(card)
 	if card_json != null:
-		get_node("Title").set_text(card_json["name"])
-		load_texture(card_json)
-	
+		if !(load_texture(card_json)):
+			get_node("Title").set_text(card_json["name"])
+		else:
+			get_node("Title").set_text("")
+
 func load_card(name):
 	var file = File.new()
-	if file.open("res://Data/Cards/"+name+".json", file.READ) != OK:
-		push_error("[Error] Opening File failed (" + "res://Data/Cards/"+name+".json" + ")")
+	var json_card_path = "res://Data/Cards/"+name+".json"
+	if file.open(json_card_path, file.READ) != OK:
+		push_error("[Error] Opening File failed (" + json_card_path + ")")
 		get_tree().quit()
 	else:
 		var text = file.get_as_text()
@@ -25,9 +29,11 @@ func load_card(name):
 func load_texture(card_json):
 	var texture = ImageTexture.new()
 	if ("texture" in card_json):
-		if texture.load("res://Assets/" + card_json["texture"] + ".png") != OK:
-			push_error("[Error] Opening File failed (" + "res://Assets/" + card_json["texture"] + ".png" + ")")
+		var image_path = "res://Assets/" + card_json["texture"] + ".png"
+		if texture.load(image_path) != OK:
+			push_error("[Error] Opening File failed (" + image_path + ")")
 			get_tree().quit()
 		else:
-			return(get_node("Sprite").set_texture(texture))
-		return (false)
+			get_node("Sprite").set_texture(texture)
+			return (1)
+	return (0)
