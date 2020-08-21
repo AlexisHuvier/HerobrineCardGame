402ee5e0614e682afcaea4df239885efde9962a6
LavaPower <lavapower84@gmail.com>
2020-08-21 15:07:45 +0200

    Create some Labels for Name, Life and Level

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var current_level = 2
 var sm = 2
+var life_ennemy = 1
+var life_player = 20
 
 onready var deck = load_json("res://Data/player.json")
 
@@ -9,6 +11,10 @@ onready var hand_node = get_node("Hand")
 onready var ennemies_node = get_node("Ennemies")
 onready var played_node = get_node("Played")
 onready var sm_node = get_node("SM")
+onready var lvl_node = get_node("LVL")
+onready var name_node = get_node("Name")
+onready var life_ennemy_node = get_node("LifeEnnemy")
+onready var life_player_node = get_node("LifePlayer")
 
 func _ready():
 	for i in range(0, 7):
@@ -17,6 +23,9 @@ func _ready():
 		deck.deck.remove(nb)
 	load_level(current_level)
 	sm_node.text = "SM : "+str(sm)
+	lvl_node.text = "Niveau : "+str(current_level+1)
+	life_player_node.text = "Vie : "+str(life_player)
+	
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -37,8 +46,12 @@ func load_level(level):
 		child.queue_free()
 	
 	var levels = load_json("res://Data/levels.json")
-	for i in range(0, levels[level].size()):
-		ennemies_node.add_child(create_card(levels[level][i], 200+i*150, 200, 0.45))
+	for i in range(0, levels[level].ennemies.size()):
+		ennemies_node.add_child(create_card(levels[level].ennemies[i], 200+i*150, 200, 0.45))
+	name_node.text = levels[level].name
+	name_node.rect_position = Vector2(640 - name_node.rect_size.x / 2, 14)
+	life_ennemy = levels[level].life
+	life_ennemy_node.text = "Vie : "+str(life_ennemy)
 
 func create_card(name, x, y, scale=0.5, rotation=0):
 	var card = load("res://Scenes/Card.tscn").instance()
