d873bbf803d2f5f7f5bda5a89bd180303ecbe645
LavaPower <lavapower84@gmail.com>
2020-09-17 22:33:09 +0200

    Change method of scrolling in Story

--- Scripts/Story.gd
+++ Scripts/Story.gd
@@ -1,19 +1,11 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var time = 0
-
 onready var text = get_node("Text")
 
-func _process(_delta):
-	if text.rect_position.y > -text.rect_size.y+800:
-		time += 1
-		if time >= 1:
-			time = 0
-			text.rect_position.y -= 1
+func _process(delta):
+	if text.rect_position.y > -text.rect_size.y+780:
+		text.rect_position.y -= 50 * delta
+			
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		if get_tree().change_scene("res://Scenes/Game.tscn") != OK:
